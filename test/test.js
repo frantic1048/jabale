1eab53d04ac5638d423d41daaa7bfeafe60d6e04
frantic1048 <archer@frantic1048.com>
2016-06-12 18:20:41 +0800

    comment,keyword,bool,int,float,[char]

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ const LEXER = path.join(__dirname, '../build/JabaleLexer');
 function match(target, input, expectedOutput, done) {
   const p = spawn(`${target}`);
   let actualOutput = '';
-  p.stdout.on('data', (data) => { actualOutput += data; })
+  p.stdout.on('data', (data) => { actualOutput += data; });
   p.on('close', () => {
     expect(actualOutput)
       .to.equal(expectedOutput);
@@ -28,11 +28,68 @@ before('Build', (done) => {
 });
 
 describe('Lexer', () => {
-  it("ONE", (done) => {
-    lexerMatch('tooo 233323', '233323\n', done);
+  describe('0x101 COMMENT', () => {
+    ['// Karada wa tsurugi dekiteiru',
+    '/* I am the bone of my sword. */',
+    '/* Steel is my body \n and fire is my blood */'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x101 COMMENT\n`, done);
+      });
+    });
   });
 
-  it("TWO", (done) => {
-    lexerMatch('2323', '2323\n', done);
+  describe('0x103 KEYWORD', () => {
+    ['abstract', 'boolean', 'break', 'byte',
+    'case', 'catch', 'char', 'class',
+    'const', 'continue', 'default', 'do',
+    'double', 'else', 'extends',
+    'final', 'finally', 'float', 'for',
+    'goto', 'if', 'implements', 'import',
+    'instanceof', 'int', 'interface', 'long',
+    'native', 'new', 'null', 'package',
+    'private', 'protected', 'public', 'return',
+    'short', 'static', 'super', 'switch',
+    'synchronized', 'this', 'throw', 'throws',
+    'transient', 'try', 'void',
+    'volatile', 'while'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x103 KEYWORD ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x105 BOOL', () => {
+    ['true', 'false'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x105 BOOL ${k}\n`, done);
+      });
+    });
+  });
+
+  xdescribe('0x106 CHAR', () => {
+    ["'z'"].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x106 CHAR ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x107 INT', () => {
+    ['123456789', '123456789L',
+    '-789', '-789L',
+    '0x789L', '-0x789L'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x107 INT ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x108 FLOAT', () => {
+    ['1.23', '0.123', '.123', '123.',
+    '123.0', '123e3', '123E3', '12.3F', '12.3f'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x108 FLOAT ${k}\n`, done);
+      });
+    });
   });
 });
