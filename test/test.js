195d71b65fb73dc5c6359b447cbd2c7f235e44de
frantic1048 <archer@frantic1048.com>
2016-06-12 21:12:40 +0800

    all tokens

--- test/test.js
+++ test/test.js
@@ -28,11 +28,19 @@ before('Build', (done) => {
 });
 
 describe('Lexer', () => {
+  describe('0x100 BADTOKEN', () => {
+    ['@', '`', '#'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x100 BADTOKEN\n`, done);
+      });
+    });
+  });
+
   describe('0x101 COMMENT', () => {
     ['// Karada wa tsurugi dekiteiru',
     '/* I am the bone of my sword. */',
     '/* Steel is my body \n and fire is my blood */'].forEach(k => {
-      it(`${k}`, (done) => {
+      it(`${k.replace('\n', '\\n')}`, (done) => {
         lexerMatch(k, `0x101 COMMENT\n`, done);
       });
     });
@@ -40,7 +48,7 @@ describe('Lexer', () => {
 
   describe('0x102 BLANK', () => {
     ['\n', ' '].forEach(k => {
-      it(`${k}`, (done) => {
+      it(`${k.replace('\n', '\\n')}`, (done) => {
         lexerMatch(k, `0x102 BLANK\n`, done);
       });
     });
@@ -62,6 +70,17 @@ describe('Lexer', () => {
     'volatile', 'while'].forEach(k => {
       it(`${k}`, (done) => {
         lexerMatch(k, `0x103 KEYWORD ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x104 IDENTIFIER', () => {
+    ['$', '_', 'a',
+    '$-', '$_', '$a', '$$',
+    '_z', '_$', '__', '_-',
+    'a-', 'a_', 'az'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x104 IDENTIFIER ${k}\n`, done);
       });
     });
   });
@@ -111,6 +130,146 @@ describe('Lexer', () => {
     '"\\t"', '"\\b"'].forEach(k => {
       it(`${k}`, (done) => {
         lexerMatch(k, `0x109 STRING ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x110 ASSIGN', () => {
+    ['=', '+=', '-=', '*=', '/=', '%=',
+    '&=', '^=', '|=',
+    '>>=', '<<=', '>>>='].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x110 ASSIGN ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x111 COND', () => {
+    ['?', ':'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x111 COND ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x112 BOOLOR', () => {
+    ['||'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x112 BOOLOR ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x113 BOOLAND', () => {
+    ['&&'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x113 BOOLAND ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x114 BITOR', () => {
+    ['|'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x114 BITOR ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x115 BITXOR', () => {
+    ['^'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x115 BITXOR ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x116 BITAND', () => {
+    ['&'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x116 BITAND ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x117 EQUAL', () => {
+    ['==', '!='].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x117 EQUAL ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x118 ORDER', () => {
+    ['<', '>', '<=', '>='].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x118 ORDER ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x119 BITSHIFT', () => {
+    ['<<', '>>', '>>>'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x119 BITSHIFT ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x11a ARITH1', () => {
+    ['+', '-'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x11a ARITH1 ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x11b ARITH2', () => {
+    ['*', '/', '%'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x11b ARITH2 ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x11c MONOARG', () => {
+    ['++', '--',
+    '!', '~'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x11c MONOARG ${k}\n`, done);
+      });
+    });
+    // TODO: Test "+" "-"
+  });
+
+  describe('0x11d EDGE', () => {
+    ['(', ')', '[', ']', '.'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x11d EDGE ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x120 COMMA', () => {
+    [','].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x120 COMMA ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x121 BRACE', () => {
+    ['{', '}'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x121 BRACE ${k}\n`, done);
+      });
+    });
+  });
+
+  describe('0x122 SEMICOLON', () => {
+    [';'].forEach(k => {
+      it(`${k}`, (done) => {
+        lexerMatch(k, `0x122 SEMICOLON ${k}\n`, done);
       });
     });
   });
